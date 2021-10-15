44ff15e599306914cf14419fcbded1afa62a9e4e
mborenst <60239769+mborenst@users.noreply.github.com>
2021-10-15 13:22:18 -0500

    HowToPlay Panel Started

---
+++ Assets/Scripts/HowToPlayController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToPlayController : MonoBehaviour
+{
+    public static HowToPlayController instance;
+    public SpriteRenderer[] casinoTutorialPictures;
+
+    private int stage;
+    private SpriteRenderer casinoMan;
+    
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        stage = 0;
+        casinoMan = casinoTutorialPictures[stage];
+    }
+
+    // Update is called once per frame
+    void nextStep()
+    {
+        stage++;
+        casinoMan = casinoTutorialPictures[stage];
+        // Other Stuff
+    }
+
+    // Update is called once per frame
+    void lastStep()
+    {
+        stage--;
+        casinoMan = casinoTutorialPictures[stage];
+        // Other Stuff
+    }
+}
