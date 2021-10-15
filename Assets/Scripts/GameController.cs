45dfdafcae4939962b31e7ba3e540895cf94d168
mborenst <60239769+mborenst@users.noreply.github.com>
2021-10-14 23:25:25 -0500

    Project Created

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    // Static Instance
+    public static GameController instance;
+
+    // Intro timer
+    public float introTime;
+    private float introTimer;
+    public SpriteRenderer introGraphic;
+    public Text introText;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(instance);
+        introTimer = introTime;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (introTimer > 0)
+        {
+            introUpdate();
+        }
+    }
+
+    void introUpdate()
+    {
+        introTimer -= 1 / 60f;
+        if (introTimer <= introTime/2f)
+        {
+            introGraphic.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, introTimer / 2.5f);
+            introText.color = new Color(255, 255, 255, introTimer / 2.5f);
+        }
+        if (introTimer <= 0)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    // Changes Scene to How To Play Scene
+    void startHowToPlay()
+    {
+
+    }
+
+    // Changes Scene to Blackjack Scene
+    void startNewGame()
+    {
+
+    }
+
+    // Changes Scene to Menu Scene
+    void startMenu()
+    {
+
+    }
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public static MenuController instance;
+
+    public Canvas mainmenu;
+    public SpriteRenderer title;
+    public Canvas controls;
+    public Canvas howToPlay;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        // mainmenu = active
+        // title = active
+        // controls = inactive
+        // howToPlay = inactive
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
